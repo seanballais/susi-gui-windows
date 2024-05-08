69ac041dfcb8cb861b2ad75efc609b89cd8a20ed
Sean Francis N. Ballais <sean@seanballais.com>
2024-05-09 00:20:22 +0800

    Make the progress bar on a file operation when the operation is done.

---
+++ GUI/FileOperationGUIUtils.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml;
+using susi_gui_windows.Core;
+
+namespace susi_gui_windows.GUI
+{
+    internal static class FileOperationGUIUtils
+    {
+        public static Visibility ShouldProgressBarBeVisible(TaskProgress state)
+        {
+            if (state != TaskProgress.Done
+                && state != TaskProgress.Failed
+                && state != TaskProgress.Interrupted)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+    }
+}
