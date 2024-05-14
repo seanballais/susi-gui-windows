e419743b9dd0d0e5b4f9e4f856ae9044affafc5d
Sean Francis N. Ballais <sean@seanballais.com>
2024-05-15 03:01:42 +0800

    Reorganize project structure to allow adding the MSIX installer project in the same folder as the app.

---
+++ susi-gui-windows/Core/Verification.cs
@@ -0,0 +1,12 @@
+﻿using susi_gui_windows.Core.Native;
+
+namespace susi_gui_windows.Core
+{
+    internal class Verification
+    {
+        public static bool IsPasswordCorrectForFile(string filePath, string password)
+        {
+            return VerificationFFI.IsPasswordCorrectForFile(filePath, password);            
+        }
+    }
+}
