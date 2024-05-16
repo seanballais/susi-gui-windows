6e89bfc199fe432eaedaf46e72196f139cf37338
Sean Francis N. Ballais <sean@seanballais.com>
2024-05-17 03:48:07 +0800

    Fix publish issues that prevents running the app from its .exe file.

--- susi-gui-windows/Core/Lib.cs
+++ susi-gui-windows/Core/Lib.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace susi_gui_windows.Core
+﻿namespace susi_gui_windows.Core
 {
     public class Lib
     {
